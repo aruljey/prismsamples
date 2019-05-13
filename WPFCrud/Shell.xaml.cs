8c1d54db4f7dbfffcdccafd4924954c425b662ce
arulrajcoder <jeyarulraj@hotmail.com>
2019-05-14 00:36:55 +0530

    Crud

--- WPFCrud/Shell.xaml.cs
+++ WPFCrud/Shell.xaml.cs
@@ -18,9 +18,9 @@ namespace WPFCrud
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Shell : Window
     {
-        public MainWindow()
+        public Shell()
         {
             InitializeComponent();
         }
